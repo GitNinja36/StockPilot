c253aa574af00d8450f4d30dea9e2ea973b36394
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-23 20:37:47 +0530

    creating about page

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-    );
+  return (
+    <div className="container mb-5">
+      <div className="row p-5 text-center border-bottom mb-5 ">
+        <h2 className="pb-4 pt-5">
+          We pioneered the discount broking model in India. <br />
+          Now, we are breaking ground with our technology.
+        </h2>
+      </div>
+      <div className="col pt-4 d-flex flex-row mb-5 p-4 justify-content-between ">
+        <div className=" col-5 p-2 medium">
+          <p className="pb-2">
+            We kick-started operations on the 15th of August, 2010 with the goal
+            of breaking all barriers that traders and investors face in India in
+            terms of cost, support, and technology. We named the company
+            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
+            barrier.
+          </p>
+          <p className="pb-2">
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India.
+          </p>
+          <p className="pb-2">
+            Over 1+ Crore clients place millions of orders every day through our
+            powerful ecosystem of investment platforms, contributing over 15% of
+            all Indian retail trading volumes.
+          </p>
+        </div>
+        <div className="col-6 p-2 medium">
+          <p className="pb-2">
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
+          <p className="pb-2">
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
+          <p className="pb-2">
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our blog or see what the media is saying about
+            us.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -1,8 +1,105 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Team() {
     return ( 
-        <h1>Team</h1>
+        <div className="mb-5">
+        <h2 className="text-center">People</h2>
+        <div className="profile-card pt-5 pb-5 mb-3 p-5 d-flex flex-row mb-3 align-items-center">
+          <div className=" profile-image-container p-5 d-flex flex-column mb-3" >
+            <img
+              src="media/images/nithinKamath.jpg"
+              alt="Nithin Kamath"
+              class="profile-image p-2"
+            />
+            <h3 className="pt-3 text-center">Nithin Kamath</h3>
+            <p className="text-center small">Founder, CEO</p>
+          </div>
+          <div className="profile-details p-5 mb-5 ">
+            <p>
+                Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
+            </p>
+            <p>
+                He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+            </p>
+            <p>
+                Playing basketball is his zen.
+            </p>
+            <p>
+                Connect on <Link to="/" className="text-decoration-none">Homepage</Link> /  <Link to="/" className="text-decoration-none">TradingQnA</Link> /  <Link to="/" className="text-decoration-none">Twitter</Link>
+            </p>
+          </div>
+        </div>
+        <div className="d-flex flex-row justify-content-around align-items-center mb-3 ">
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Nikhil.jpg"
+                alt="Nikhil"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Nikhil Kamath</h4>
+            <p className="text-center small">Co-founder & CFO</p>
+          </div>
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Kailash.jpg"
+                alt="Kailash"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Dr. Kailash Nadh</h4>
+            <p className="text-center small">CTO</p>
+          </div>
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Venu.jpg"
+                alt="Venu"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Venu Madhav</h4>
+            <p className="text-center small">COO</p>
+          </div>
+        </div>
+        <div className="d-flex flex-row justify-content-around align-items-center mb-3 ">
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Hanan.jpg"
+                alt="Hanan"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Hanan Delvi</h4>
+            <p className="text-center small">CCO</p>
+          </div>
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Seema.jpg"
+                alt="Seema"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Seema Patil</h4>
+            <p className="text-center small">Director</p>
+          </div>
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/karthik.jpg"
+                alt="karthik"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Karthik Rangappa</h4>
+            <p className="text-center small">Chief of Education</p>
+          </div>
+        </div>
+        <div className="d-flex flex-row justify-content-around align-items-center mb-3 ">
+          <div className="d-flex flex-column mb-3">
+            <img 
+                src="media/images/Austin.jpg"
+                alt="Austin Prakesh"
+                class="team-profile-image p-2"
+            />
+            <h4 className="text-center pt-2">Austin Prakesh</h4>
+            <p className="text-center small">Director Strategy</p>
+          </div>
+        </div>
+      </div>
     );
 }
 
