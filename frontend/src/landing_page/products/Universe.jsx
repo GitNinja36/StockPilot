75e8a1284dbde61fb7760bea4d6a35655eb1b60b
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-24 23:14:51 +0530

    updating Product Tag

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -19,29 +19,78 @@ function Universe() {
             partner platforms
           </p>
         </div>
-        {/* <div className="d-flex flex-column justify-content-around align-items-center mb-3">
-          <div>
-            <div>
-              <img src="media/images/zerodhaFundhouse.png" className="" />
-            </div>
-            <div>
-              <p className="small">
-                Our asset management venture that is creating simple and
-                transparent index funds to help you save for your goals.
-              </p>
-            </div>
+        <div className="d-flex flex-row justify-content-evenly mb-3 text-muted small">
+          <div className="p-3"></div>
+          <div className="text-center">
+            <img
+              src="media/images/zerodhaFundhouse.png"
+              style={{ height: "4rem"}}
+            />
+            <p className="small p-3 m-2 ">
+              Our asset management venture that is creating simple and
+              transparent index funds to help you save for your goals.
+            </p>
           </div>
-          <div></div>
-          <div></div>
+          <div className="text-center">
+            <img
+              src="media/images/sensibullLogo.svg"
+              style={{ height: "3rem", marginTop:"1px" }}
+            />
+            <p className="small p-3 m-2">
+              Options trading platform that lets you create strategies, analyze
+              positions, and examine data points like open interest, FII/DII,
+              and more.
+            </p>
+          </div>
+          <div className="text-center pb-3">
+            <img
+              src="media/images/tijori.svg"
+              style={{ height: "4rem" }}
+            />
+            <p className="small p-3 m-2">
+              Investment research platform that offers detailed insights on
+              stocks, sectors, supply chains, and more.
+            </p>
+          </div>
+          <div className="p-3"></div>
         </div>
-        <div>
+        <div className="p-3"></div>
+        <div className="d-flex flex-row justify-content-evenly mb-3 text-muted small">
+          <div className="p-3"></div>
+          <div className="text-center">
+            <img src="media/images/streakLogo.png" style={{ height: "4rem" }} />
+            <p className="small p-3 m-2">
+              Systematic trading platform that allows you to create and backtest
+              strategies without coding.
+            </p>
+          </div>
+          <div className="text-center">
+            <img
+              src="media/images/smallcaseLogo.png"
+              style={{ height: "4rem" }}
+            />
+            <p className="small p-3 m-2">
+              Thematic investing platform that helps you invest in diversified
+              baskets of stocks on ETFs.
+            </p>
+          </div>
+          <div className="text-center">
+            <img src="media/images/dittoLogo.png" style={{ height: "3rem" }} />
+            <p className="small p-3 m-2">
+              Personalized advice on life and health insurance. No spam and no
+              mis-selling.
+            </p>
+          </div>
+          <div className="p-3"></div>
+        </div>
+        <div className="text-center pb-5 pt-5">
           <button
-            className="p-2 btn btn-primary fs-5 mt-4"
+            className="p-2 btn btn-primary fs-5 mt-4 "
             style={{ width: "25%", margin: "0 auto" }}
           >
             Sign up for free
           </button>
-        </div> */}
+        </div>
       </div>
     </div>
   );
