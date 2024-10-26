8a945bb0c160cd5742dbd4a00ab9991633f2bfcf
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-26 14:19:28 +0530

    adding the hero tag on support page

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -1,9 +1,90 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CreateTicket() {
-    return ( 
-        <h1>CreateTicket</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h5 className="fs-5 text-muted">To create a ticket, select a relevant topic</h5>
+        <div className="col-4 p-5">
+          <h4 className="">
+                Account Opening
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Getting Started</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Online</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Offline</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Charges</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Company, Partnership and HUF</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Non Resident Indian (NRI)</Link>
+          </div>
+        </div>
+        <div className="col-4 p-5 ">
+          <h4 className="">
+          Your Zerodha Account 
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Login credentials</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Your Profile</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Account modification and segment addition</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >CMR & DP ID</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Nomination</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Transfer and conversion of shares</Link>
+          </div>
+        </div>
+        <div className="col-4 p-5 ">
+          <h4 className="">
+          Trading and Markets
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Trading FAQs</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Kite</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Margins</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Product and order types</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Corporate actions</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Kite features</Link>
+          </div>
+        </div>
+        <div className="col-4 p-5 ">
+          <h4 className="">
+          Funds
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Fund withdrawal</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Adding funds</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Adding bank accounts</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >eMandates</Link>
+          </div>
+        </div>
+        <div className="col-4 p-5 ">
+          <h4 className="">
+          Console
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >IPO</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Portfolio</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Funds statement</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Profile</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Reports</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Referral program</Link>
+          </div>
+        </div>
+        <div className="col-4 p-5">
+          <h4 className="">
+          Coin
+          </h4>
+          <div className="p-3 small">
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Understanding mutual funds and Coin</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Coin app</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Funds statement</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Coin web</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >Transactions and reports</Link><br />
+            <Link to="" style={{ textDecoration: "none", lineHeight: "2.5" }} >National Pension Scheme (NPS)</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='conatiner' style={{  backgroundColor:"rgb(56, 126, 209)", color:"white"}}>
+            <div className='row'>
+                <div className='row p-5'>
+                    <div className='col-1'></div>
+                    <h4 className='col-2 fw-medium'>Support Portal</h4>
+                    <div className='col-7'></div>
+                    <p className='col-2' ><Link to=""  style={{color:"white"}}>Track tickets</Link></p>
+                </div>
+                <div className='d-flex flex-row p-4 mb-5 mt-3 justify-content-between'>
+                    <div className='pl-3' ></div>
+                    <div className='d-flex flex-column mb-3'>
+                        <h2 className='mb-5'>Search for an answer or browse help topics to create a ticket</h2>
+                        <input type="text" className='mb-4 p-2' />
+                        <div className='d-flex flex-row mb-3 pl-5'>
+                        <p><Link to="" style={{color:"white", marginLeft:"2rem"}}>Track account opening  </Link></p>
+                        <p><Link to="" style={{color:"white", marginLeft:"2rem"}}>Track segment activation </Link></p>
+                        <p><Link to="" style={{color:"white", marginLeft:"2rem"}}>Intraday margins</Link></p>
+                        <p><Link to="" style={{color:"white", marginLeft:"2rem"}}> Kite user manual</Link></p></div>
+                    </div>
+                    <div className='d-flex flex-column  mb-3'>
+                        <h3>Featured</h3>
+                        <div className='p-4'>
+                            <p>1. <Link to="" style={{color:"white"}}>Rights Entitlements listing in October 2024</Link></p>
+                            <p>2. <Link to="" style={{color:"white"}}>Surveillance measure on scrips - October 2024</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
