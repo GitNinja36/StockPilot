51118a4bf89b20318ec071eeeade53ec75aa24ab
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-12-13 14:33:18 +0530

    adding login route

--- dashboard/src/components/LoggedIn.jsx
+++ dashboard/src/components/LoggedIn.jsx
@@ -1,10 +1,93 @@
-import React from "react";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import { handleError, handleSuccess } from './utils';
 
-function LoggedIn(){
-    return(
-        <>
-        </>
-    );
-};
+function Login() {
 
-export default LoggedIn;
+    const [loginInfo, setLoginInfo] = useState({
+        email: '',
+        password: ''
+    })
+
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value);
+        const copyLoginInfo = { ...loginInfo };
+        copyLoginInfo[name] = value;
+        setLoginInfo(copyLoginInfo);
+    }
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const {email, password } = loginInfo;
+        if (!email || !password) {
+            return handleError('email and password are required')
+        }
+        try {
+            const url = `http://localhost:8080/auth/login`;
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginInfo)
+            });
+            const result = await response.json();
+            const { success, message, name, jwtToken,  error } = result;
+            if (success) {
+                handleSuccess(message);
+                localStorage.setItem('token', jwtToken);
+                localStorage.setItem('loggedInUser', name);
+                setTimeout(() => {
+                    navigate('/')
+                }, 1500)
+            } else if (error) {
+                const details = error?.details[0].message;
+                handleError(details);
+            } else if (!success) {
+                handleError(message);
+            }
+            console.log(result);
+        } catch (err) {
+            handleError(err);
+        }
+    }
+    return (
+        <div className='container'>
+            <h1 style={{textAlign:"center"}}>Login</h1>
+            <form onSubmit={handleLogin} className='signinContainer'>
+                <div className='nameContainer'>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        onChange={handleChange}
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email...'
+                        value={loginInfo.email}
+                        className='signinInput'
+                    />
+                </div>
+                <div className='nameContainer'>
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        onChange={handleChange}
+                        type='password'
+                        name='password'
+                        placeholder='Enter your password...'
+                        value={loginInfo.password}
+                        className='signinInput'
+                    />
+                </div>
+                <button type='submit' className='submitButton'>Login</button>
+                <span className='signIntext'>If you don't have an account ? 
+                    <Link to="/signup">Signup</Link>
+                </span>
+            </form>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default Login;
